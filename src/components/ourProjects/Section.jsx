2ce6477c601fde1e300d78e3667de1198d1d5214
“deepchandra02” <thedeepchandra@gmail.com>
2022-07-15 00:36:16 +0300

    complete overhaul of projects data management

--- src/components/ourProjects/Section.jsx
+++ src/components/ourProjects/Section.jsx
@@ -1,32 +1,41 @@
 import React from "react";
+import data from "./projectsData.tsx";
 import Headings from "../Headings";
-import defaultImage from "../Zimages/default.jpeg";
-import project1 from "../Zimages/project1.png";
-import project2 from "../Zimages/project2.png";
-import project3 from "../Zimages/project3.png";
-import project4 from "../Zimages/project4.png";
-import project5 from "../Zimages/project5.png";
-import project6 from "../Zimages/project6.png";
+import defaultImage from "../Zimages/default.png";
 
 function Projects(props) {
   return (
-    <div class="relative flex mx-auto fit-content font-montserrat">
-      <div class="flex flex-col absolute inset-0 pl-4 pb-4 z-10 bg-black text-white justify-end opacity-0 hover:opacity-100 bg-opacity-60 duration-300">
-        <h3 class="font-semibold text-base sm:text-sm">{props.service}</h3>
-        <h4 class="font-light text-base sm:text-sm">{props.name}</h4>
-        <h4 class="mt-2 font-light text-base sm:text-sm">{props.location}</h4>
-      </div>
-      <div class="relative">
-        <div class="">
-          <img src={props.image} alt="project" className="max-h-72 sm:h-auto" />
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-6 sm:px-6 md:px-10 lg:px-14 my-12">
+      {data.map((item) => (
+        <div class="relative flex mx-auto fit-content font-montserrat">
+          <div class="flex flex-col absolute inset-0 pl-4 pb-4 z-10 bg-black text-white justify-end opacity-0 hover:opacity-100 bg-opacity-60 duration-300">
+            <h3 class="font-semibold text-base sm:text-sm">
+              {item.service ? item.service : props.service}
+            </h3>
+            <h4 class="font-light text-base sm:text-sm">
+              {item.name ? item.name : props.name}
+            </h4>
+            <h4 class="mt-2 font-light text-base sm:text-sm">
+              {item.location ? item.location : props.location}
+            </h4>
+          </div>
+          <div class="relative">
+            <div class="">
+              <img
+                src={item.image ? item.image : props.image}
+                alt="project"
+                className="max-h-72 sm:h-auto"
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
 
 Projects.defaultProps = {
-  service: "Facilities Management",
+  service: "Service Type",
   name: "Project Name",
   location: "Location",
   image: defaultImage,
@@ -45,27 +54,8 @@ const Section = () => {
           buildings, yet we are expanding.
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-6 sm:px-6 md:px-10 lg:px-14 my-12">
-        <Projects
-          service="Facilities Management"
-          name="Qatar National Bank"
-          location="Doha - Qatar"
-          image={project1}
-        />
-        <Projects image={project2} />
-        <Projects image={project3} />
-        <Projects image={project4} />
-        <Projects image={project5} />
-        <Projects image={project6} />
-        <Projects />
-        <Projects />
-        <Projects />
-        <Projects />
-        <Projects />
-        <Projects />
-        <Projects />
-        <Projects />
-      </div>
+
+      <Projects />
     </div>
   );
 };
