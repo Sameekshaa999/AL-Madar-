48c2e996f4c355fb4c62bedaa52715910ef893ca
“deepchandra02” <thedeepchandra@gmail.com>
2022-08-05 13:10:45 +0300

    added react-revel, with example

--- src/components/index/Hero.jsx
+++ src/components/index/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import hero from "../Zimages/index/1.1.jpg";
 import heroMobile from "../Zimages/index/hero_mobile.jpg";
+import { Fade } from "react-reveal";
 
 const Hero = () => {
   return (
@@ -17,27 +18,28 @@ const Hero = () => {
         className="md:hidden top-6.5 w-full z-0"
       />
       <div className="mx-auto absolute mt-24">
-        <h1 className=" text-center text-mpurple font-extrabold px-20 md:px-0 text-3xl sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl mb-12 lg:mb-16">
-          Exceeding Expectations <br></br>One Property at A Time
-        </h1>
+        <Fade top>
+          <h1 className=" text-center text-mpurple font-extrabold px-20 md:px-0 text-3xl sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl mb-12 lg:mb-16">
+            Exceeding Expectations <br></br>One Property at A Time
+          </h1>
+        </Fade>
         <div className="flex justify-center items-center space-x-4">
-        <button>
-          <Link
-            to="/contact"
-            className="text-center px-4 md:px-6 py-2 md:py-3 text-mpurple font-semibold border-3 border-mpurple rounded-full md:rounded-lg text-sm md:text-2xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out "
-          >
-            Contact Us
-          </Link>
+          <button>
+            <Link
+              to="/contact"
+              className="text-center px-4 md:px-6 py-2 md:py-3 text-mpurple font-semibold border-3 border-mpurple rounded-full md:rounded-lg text-sm md:text-2xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out "
+            >
+              Contact Us
+            </Link>
           </button>
           <button>
-          <Link
-            to="/contact"
-            className="text-center px-4 md:px-6 py-2 md:py-3 text-mpurple font-semibold border-3 border-mpurple rounded-full md:rounded-lg text-sm md:text-2xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out "
-          >
-            Know More
-          </Link>
+            <Link
+              to="/contact"
+              className="text-center px-4 md:px-6 py-2 md:py-3 text-mpurple font-semibold border-3 border-mpurple rounded-full md:rounded-lg text-sm md:text-2xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out "
+            >
+              Know More
+            </Link>
           </button>
-          
         </div>
       </div>
     </div>
